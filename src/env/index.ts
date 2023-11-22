34df071d3c48dd2c7e340f41ad18b0d0e7db137c
DanielRPTI <drjunior988@hotmail.com>
2023-11-22 14:43:58 -0300

    Feat: Criação da tabela e listagem de todas as meals criadas

---
+++ src/env/index.ts
@@ -0,0 +1,19 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+// Cria a tipagem para todas as enverioment variables para utilizar de uma forma mais facíl o typescript
+const envSchema = z.object({
+  NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
+  DATABASE_URL: z.string(),
+  PORT: z.coerce.number().default(3000),
+})
+
+// Usando o SafePArse para tratar os erros e validar os mesmo de uma forma melhor:
+const _env = envSchema.safeParse(process.env)
+
+if (!_env.success) {
+  throw new Error('⚠️ Invalid env variables')
+}
+
+export const env = _env.data
--- src/routes/dailyDiet.ts
+++ src/routes/dailyDiet.ts
@@ -1,16 +1,48 @@
 import { FastifyInstance } from 'fastify'
 import { knex } from '../database'
+import { z } from 'zod'
+import { checkUserId } from '../middlewares/check-user-id'
+import { randomUUID } from 'crypto'
 
 export async function dailyDietRoutes(app: FastifyInstance) {
-  app.get('/user', async () => {
-    const user = await knex('users')
-      .insert({
-        id: crypto.randomUUID(),
-        username: 'Daniel',
-        age: 21,
-      })
-      .returning('*')
+  app.addHook('preHandler', checkUserId)
+  // Lista todas as refeições cadastradas
+  app.get('/', async (request, reply) => {
+    const { user_id } = request.cookies
+    const listAllMealsCreated = await knex('meals')
+      .where({ user_id })
+      .select('*')
 
-    return user
+    return {
+      listAllMealsCreated,
+    }
+  })
+
+  app.post('/', async (request, reply) => {
+    // pega o cookie de identificação de sessão do usuario
+    const { user_id } = request.cookies
+
+    // Tipagem dos dados presente no corpo da requisição
+    const creatMealBodySchema = z.object({
+      name: z.string(),
+      description: z.string(),
+      within_diet: z.boolean(),
+      consumed_at: z.string(),
+    })
+
+    const { name, description, within_diet, consumed_at } =
+      creatMealBodySchema.parse(request.body)
+
+    // Cria na tabela uma refeição feita por um determinado usuario:
+
+    await knex('meals').insert({
+      id: randomUUID(),
+      name,
+      description,
+      within_diet,
+      user_id,
+      consumed_at,
+    })
+    return reply.status(201).send()
   })
 }
