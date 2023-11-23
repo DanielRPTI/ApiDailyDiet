0c982a225217c7d0e9d447deb2686f69f9be23b9
DanielRPTI <drjunior988@hotmail.com>
2023-11-23 16:52:40 -0300

    chore:Criando ambiente de teste e validações

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,6 +1,11 @@
-import 'dotenv/config'
+import { config } from 'dotenv'
 import { z } from 'zod'
 
+if (process.env.NODE_ENV === 'test') {
+  config({ path: '.env.test' })
+} else {
+  config()
+}
 // Cria a tipagem para todas as enverioment variables para utilizar de uma forma mais facíl o typescript
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
---
+++ test/dailyDiet.spec.ts
@@ -0,0 +1,4 @@
+import { describe, test } from 'vitest'
+import supertest from 'supertest'
+import { app } from '../src/app'
+describe('Meals Route', () => {})
