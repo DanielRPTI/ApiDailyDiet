cd37b974d27c6a32d01473b35be3e3b814ecc54c
DanielRPTI <drjunior988@hotmail.com>
2023-11-24 10:22:36 -0300

    feat: Criando mais um teste para listar todas meals

--- test/dailyDiet.spec.ts
+++ test/dailyDiet.spec.ts
@@ -1,4 +1,4 @@
-import { afterAll, beforeAll, describe, it, beforeEach } from 'vitest'
+import { afterAll, beforeAll, describe, it, beforeEach, expect } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app'
 import { execSync } from 'child_process'
@@ -13,7 +13,7 @@ describe('Meals Process Route', () => {
     execSync('npm run knex migrate:rollback --all')
     execSync('npm run knex migrate:latest')
   })
-  it.only('Sould be able to creat a User', async () => {
+  it('Sould be able to creat a User', async () => {
     await request(app.server)
       .post('/users')
       .send({
@@ -42,5 +42,35 @@ describe('Meals Process Route', () => {
         consumed_at: '2023-11-21 10:40:00',
       })
       .expect(201)
+  })
+
+  it.only('Should be able to list all Meal', async () => {
+    const createUserResponse = await request(app.server).post('/users').send({
+      username: 'Daniel Rosa',
+      age: 21,
+    })
+
+    const userId = createUserResponse.get('Set-Cookie')
+
+    await request(app.server).post('/meals').set('Cookie', userId).send({
+      user_id: userId,
+      name: 'Jantar',
+      description: 'Arroz, salada, feijao e frango',
+      within_diet: true,
+      consumed_at: '2023-11-21 10:40:00',
+    })
+    const listAllMealsResponse = await request(app.server)
+      .get('/meals')
+      .set('Cookie', userId)
+      .expect(200)
+
+    expect(listAllMealsResponse.body.meals).toEqual([
+      expect.objectContaining({
+        name: 'Jantar',
+        description: 'Arroz, salada, feijao e frango',
+        within_diet: 1,
+        consumed_at: '2023-11-21 10:40:00',
+      }),
+    ])
   })
 })
