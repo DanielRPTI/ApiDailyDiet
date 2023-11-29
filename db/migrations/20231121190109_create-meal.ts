770c901da326991db34f28a941c8bb9fdfd127fc
DanielRPTI <drjunior988@hotmail.com>
2023-11-29 10:42:25 -0300

    fix: consertando foreign key da tabela users

--- db/migrations/20231121190109_create-meal.ts
+++ db/migrations/20231121190109_create-meal.ts
@@ -3,7 +3,7 @@ import { Knex } from 'knex'
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('meals', (table) => {
     table.uuid('id').notNullable()
-    table.uuid('user_id').references('user.id').index().notNullable()
+    table.uuid('user_id').references('users.id').index().notNullable()
     table.text('name').notNullable()
     table.text('description').notNullable()
     table.timestamp('consumed_at').defaultTo(knex.fn.now())
